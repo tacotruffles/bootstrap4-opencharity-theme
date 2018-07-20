2a4e7341d5bd6c6b44ecc6636535cd2c10dc2caf
John Dawes <jdawes@bluebitmedia.com>
2018-07-20 16:37:48 -0600

    Events / Blog roll / Contact / Footer

--- assets/style/js/main.js
+++ assets/style/js/main.js
@@ -19,4 +19,55 @@ $(document).ready(function() {
       $("#navbar").removeClass("navbar-shrink");
     }
   });
+
+  // Init Owl Sliders
+  // Members Slider
+  $("#members-slider").owlCarousel({
+    margin: 25,
+    smartSpeed: 1000,
+    nav: false,
+    dots: true,
+    dotsEach: true,
+    loop: true,
+    autoplay: true,
+    mouseDrag: true,
+    touchDrag: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  });
+
+  // Blog Slider
+  $("#blog-slider").owlCarousel({
+    margin: 20,
+    smartSpeed: 1000,
+    nav: true,
+    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+    loop: true,
+    autoplay: true,
+    mouseDrag: true,
+    touchDrag: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
 });
